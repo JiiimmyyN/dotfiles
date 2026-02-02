d164ed25e9575fb4f9b80c7ed4bdabe11e8f3305
Jimmy Nilsson <jiiiimmmyy@gmail.com>
2026-02-02 12:37:52 +0100

    feat: add 99 plugin

---
+++ nvim/.config/nvim/lua/plugins/extras.lua
@@ -0,0 +1,7 @@
+return {
+  {
+    import = "lazyvim.plugins.extras.coding.nvim-cmp",
+  },
+  -- Disable blink.cmp (LazyVim default) so nvim-cmp works for 99 plugin
+  { "saghen/blink.cmp", enabled = false },
+}
